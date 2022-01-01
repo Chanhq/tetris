5716d9f54a9785f876891585de125d93783bf734
oxor <carlroccobecker@gmail.com>
2022-01-01 20:01:37 +0100

    Addes basic files

---
+++ SDL Test/sdltest.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "SDL.h"
+
+
+int main(void) {
+    if (SDL_Init(SDL_INIT_VIDEO|SDL_INIT_TIMER) != 0) {
+        fprintf(stderr, "\nUnable to initialize SDL: Â %s\n", SDL_GetError());
+        return 1;
+    }
+    atexit(SDL_Quit);
+
+
+    return 0;
+} 
